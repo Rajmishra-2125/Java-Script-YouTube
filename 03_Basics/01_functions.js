d1abdf9eb043279f53352794d71e7dcdaaa22181
Raj Mishra <raj212558@gmail.com>
2025-06-06 12:38:09 +0000

    update it

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -16,7 +16,7 @@ SayMyName();
 
 // function addTwoNumbers (number1, number2) {
 //     // console.log(number1 + number2);   // 10
-// }
+// };
 
 // const result = addTwoNumbers(3,7);  
 // console.log("Result :",  result);  // Result : undefined
@@ -27,7 +27,7 @@ SayMyName();
 function addTwoNumbers (number1, number2) {
       let result = number1 + number2
       return result
-}
+};
 const result = addTwoNumbers(3,7);
 console.log(result);   // 10
 
@@ -36,7 +36,7 @@ console.log(result);   // 10
 
 function addTwoNumbers (number1, number2) {
       return  number1 + number2
-}
+};
 const result = addTwoNumbers(3,7);
 // console.log(result);
 
@@ -51,10 +51,10 @@ function loginUserMessage(userName){
        return
   }
   return `${userName} just logged In `
-}
+};
 
 // console.log(loginUserMessage("Raj Mishra"));
-console.log(loginUserMessage());   //  undefined
+// console.log(loginUserMessage());   //  undefined
 
 // we can define same syntax in another way
 
@@ -64,7 +64,50 @@ function loginUserMessage(userName){
        return
   }
   return `${userName} just logged In `
-}
+};
 
-console.log(loginUserMessage("Raj Mishra"));   // Raj Mishra just logged In
+// console.log(loginUserMessage("Raj Mishra"));   // Raj Mishra just logged In
 // console.log(loginUserMessage());
+
+
+
+/* ++++++++++++++++++++++++++++++++++++ Functions with Objects ++++++++++++++++++++++++++ */
+
+// function calculateCartPrice(num1){
+//    return num1
+// };
+
+// console.log(calculateCartPrice(2));
+
+
+
+function calculateCartPrice(...num1){
+   return num1
+};
+
+// console.log(calculateCartPrice(200, 400, 800, 2000));   // [ 200, 400, 800, 2000 ]
+
+const user = {
+  userName : "Raj Mishra",
+  price : 199
+};
+
+function handleObjects(anyObjects){
+   console.log(`UserName is ${anyObjects.userName} and price is ${anyObjects.price}`);
+} ;
+
+// handleObjects(user);  // UserName is Raj Mishra and price is 199
+handleObjects({userName: "Rajan", price: 1999});  // UserName is Rajan and price is 1999
+
+
+/* ++++++++++++++++++++++++++++++++++++ Functions with Arrays ++++++++++++++++++++++++++ */
+
+
+const myNewArray = [200, 300, 400, 2000];
+
+function getNewArray(anyArray){
+      return anyArray[1]  
+};
+
+// console.log(getNewArray(myNewArray));    // 300
+console.log(getNewArray([200, 400, 700, 1000]));  // 400
