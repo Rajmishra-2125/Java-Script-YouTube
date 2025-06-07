6cde162320e62655417b7b6733a6b2344c1ed235
Raj Mishra <raj212558@gmail.com>
2025-06-07 10:25:35 +0000

    update it

---
+++ 04_Control_Flow/one.js
@@ -0,0 +1,53 @@
+// if statement
+
+const isLoggedIn = true;
+const temperature = 41;
+
+// if( temperature > 50){
+//     console.log("Temperature is greater than 50");   
+// }
+// else{
+//     console.log(`Temperature is ${temperature}`);  // Temperature is 41
+// };
+
+// ＞，<, <=，>=，==，!=，===，!==
+
+const score = 200;
+
+// if (score > 100){
+//     const power = "fly";
+ //   console.log(`user Power : ${power}`);
+// }else {
+//    console.log(`happy ending`);
+// };
+
+const balance = 1000;
+// if (balance > 500) console.log("test"), console.log("test2");   // never use this type
+
+// if (balance < 500){
+//     console.log("less than 500");
+// }else if (balance < 750){
+//     console.log("less than 750");
+// }else if (balance < 900){
+//     console.log("less than 900");
+// }else{ 
+//     console.log("less than 1500");
+// };
+
+
+const userLoggedIn = true;
+const debitcard = true;
+const loggedInFromGoogle = false;
+const logggedInFromEmail = true;
+
+// if (userLoggedIn && debitcard){
+//     console.log("You can do shoping");   //  You can do shoping
+// }else{
+//     console.log("You can't do shoping");   
+// };
+
+// if (loggedInFromGoogle || logggedInFromEmail){
+//     console.log("You are logged in");  //  You are logged in
+// }else{
+//     console.log("You are not logged in");
+// };
